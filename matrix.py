724a483371d3e91c62d0a2e3dfc9fadb88cccced
Jenny Gao <jgao658@gmail.com>
2018-03-01 11:25:03 -0500

    added rotation matrices

--- matrix.py
+++ matrix.py
@@ -3,27 +3,53 @@ import math
 def make_translate( x, y, z ):
     m=new_matrix(4,4)
     i=ident(m)
-    m[3]=[x,y,z,1]
+    m[3]=[x,y,z,1.0]
     return m
 
 def make_scale( x, y, z ):
     m=new_matrix(4,4)
     i=ident(m)
-    m[0]=[x,0,0,0]
-    m[1]=[0,y,0,0]
-    m[2]=[0,0,z,0]
-    m[3]=[0,0,0,1]
+    m[0]=[x,0.0,0.0,0.0]
+    m[1]=[0.0,y,0.0,0.0]
+    m[2]=[0.0,0.0,z,0.0]
+    m[3]=[0.0,0.0,0.0,1]
     return m
 
 def make_rotX( theta ):
+    m=new_matrix(4,4)
+    i=ident(m)
     rad=math.radians(theta) ##angle in radians
     cosa=math.cos(rad)
-
+    sina=math.sin(rad)
+    m[0]=[1,0.0,0.0,0.0]
+    m[1]=[0.0,cosa,sina,0.0]
+    m[2]=[0.0,sina*-1,cosa,0.0]
+    m[3]=[0.0,0.0,0.0,1.0]
+    return m
+    
 def make_rotY( theta ):
-    pass
+    m=new_matrix(4,4)
+    i=ident(m)
+    rad=math.radians(theta) ##angle in radians
+    cosa=math.cos(rad)
+    sina=math.sin(rad)
+    m[0]=[cosa,0.0,sina*-1,0.0]
+    m[1]=[0.0,1.0,0.0,0.0]
+    m[2]=[sina,0.0,cosa,0.0]
+    m[3]=[0.0,0.0,0.0,1.0]
+    return m
 
 def make_rotZ( theta ):
-    pass
+    m=new_matrix(4,4)
+    i=ident(m)
+    rad=math.radians(theta) ##angle in radians
+    cosa=math.cos(rad)
+    sina=math.sin(rad)
+    m[0]=[cosa,sina,0.0,0.0]
+    m[1]=[sina*-1,cosa,0.0,0.0]
+    m[2]=[0.0,0.0,1,0.0]
+    m[3]=[0.0,0.0,0.0,1.0]
+    return m
 
 def print_matrix( matrix ):
     s = ''
@@ -37,9 +63,9 @@ def ident( matrix ):
     for r in range( len( matrix[0] ) ):
         for c in range( len(matrix) ):
             if r == c:
-                matrix[c][r] = 1
+                matrix[c][r] = 1.0
             else:
-                matrix[c][r] = 0
+                matrix[c][r] = 0.0
 
 def scalar_mult( matrix, s ):
     for r in range( len( matrix[0] ) ):
@@ -67,10 +93,11 @@ def new_matrix(rows = 4, cols = 4):
     for c in range( cols ):
         m.append( [] )
         for r in range( rows ):
-            m[c].append( 0 )
+            m[c].append( 0.0 )
     return m
 
 ###TEST
 #print_matrix(make_translate(1,2,3))
-print make_translate(1,2,3)
-print make_scale(1,2,3)
+#print make_translate(1,2,3)
+#print make_scale(1,2,3)
+#print print_matrix(make_rotZ(30))
